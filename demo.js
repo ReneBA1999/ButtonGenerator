e7e9ee61e24c75c441297336cfd34b024b1b68db
Rene Banuelos <rene.b.arellano7@gmail.com>
2024-05-02 10:04:17 -0700

    Generate names button
    Generate names button

--- demo.js
+++ demo.js
@@ -1,23 +1,24 @@
-function generateIcebreaker() {
-  const questions = [
-    "What's your favorite way to unwind after a long day of coding?",
-    "What hobby would you get into if time and money weren't an issue?",
-    "What's the last book you read or movie you watched that you really enjoyed?",
-    "If you could visit any country in the world, where would you go and why?",
-    "What's one skill you'd like to develop or hobby you'd like to take up?",
-    "What's the most memorable vacation you've ever had?",
-    "If you could have dinner with any historical figure, who would it be and why?",
-    "What's your favorite season of the year and why?",
-    "What’s your favorite dish to cook or eat?",
-    "If you could instantly become an expert in something, what would it be?",
+function generateNames() {
+  const names = [
+    "Hello Brett",
+    "Hello Uriel",
+    "Hello Kayla",
+    "Hello Jesus",
+    "Hello Daniel",
+    "Kaaliyah",
+    "Hello Victor",
+    "Hello Alfi",
+    "Hello Donny",
+    "Hello Abdi",
+    "Hello Kadi"
   ];
 
   // Randomly select a question
-  const index = Math.floor(Math.random() * questions.length);
+  const index = Math.floor(Math.random() * names.length);
 
-  document.getElementById("displayQuestion").innerHTML = questions[index];
+  document.getElementById("displayQuestion").innerHTML = names[index];
   // return questions[index];
 }
 
 // Example usage:
-let thequestion = generateIcebreaker();
+let thequestion = generateNames();
