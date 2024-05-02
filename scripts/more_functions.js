b4c717b59e7b8f0f072af43faa1d4f049d7583da
Rene Banuelos <rene.b.arellano7@gmail.com>
2024-05-02 15:14:30 -0700

    save examples
    save examples

---
+++ scripts/more_functions.js
@@ -0,0 +1,19 @@
+"use strict"
+
+function displayMailingLabel(name, address, city, state, zip) {
+    let display = "Name:" + name + "Address:" + address + "City:" + city + "State:" + state + "Zip:" + zip;
+    console.log(display);
+}
+
+let name = " Jhon Doe ";
+let address = " 123 E elm street ";
+let city = " Phoenix ";
+let state = " Az ";
+let zip = " 85008 ";
+
+displayMailingLabel(name, address, city, state, zip);
+
+function addNumbers(num1, num2){
+let  someNumber = num1 + num2 '=' num3;
+}
+let num3 = num1 + num2;
